bbaedce596ae1479cab2208349a7fea408b1db5d
kaboo <pm.kaboo@gmail.com>
2013-09-05 21:56:04 +0200

    add env vars support, setup db:seed task, create db:clear and heroku:set_vars tasks, update readme

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,8 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+countries = [
+    {name: "kazachstan", coordinates: "545,114,576,114,574,105,594,100,616,104,646,119,638,138,610,143,596,141,572,132,568,145,557,137,562,133,559,129,552,129,541,122,543,116,544,116"},
+    {name: "greenland", coordinates: "298,25,376,46"}
+]
+
+countries.each do |country|
+  Country.where(country).first_or_create
+end
