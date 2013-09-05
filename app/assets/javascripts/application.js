dd00734e1192a3c8acf1314388c113248c469926
kaboo <pm.kaboo@gmail.com>
2013-09-05 22:06:25 +0200

    create template solution

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,9 +14,21 @@
 //= require jquery_ujs
 //= require_tree .
 
+
+jQuery.fn.center = function () {
+    this.css("position","absolute");
+    this.css("top", Math.max(0, (($(window).height() - $(this).outerHeight()) / 2) +
+        $(window).scrollTop()) + "px");
+    this.css("left", Math.max(0, (($(window).width() - $(this).outerWidth()) / 2) +
+        $(window).scrollLeft()) + "px");
+    return this;
+};
+
 $(document).ready(function(){
     $('#c_nav li').click(function() {
         $(this).siblings('li').removeClass('active');
         $(this).addClass('active');
     });
+
+    $("#country-info").center();
 });
--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,2 +1,19 @@
 module PagesHelper
+
+  def map_area country
+    ret = <<-HTML
+      <area shape="#{shape country.coordinates}" coords="#{country.coordinates}"
+      href="#" onclick="$('##{country.name}').trigger('click'); return false" />
+    HTML
+    ret.html_safe
+  end
+
+  def map_link country
+    link_to country.name, pages_country_path(country.name), remote: true, id: country.name
+  end
+
+  def shape coords
+    coords.split(",").size == 4 ? "rect" : "poly"
+  end
+
 end
